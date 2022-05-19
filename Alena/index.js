000076cc8dc2039857e16bc9e6250f6f0d0db442
Abu! <100409086+Afx-Abu@users.noreply.github.com>
2022-05-19 17:30:25 +0530

    Fix base and problems

--- Alena/index.js
+++ Alena/index.js
@@ -1,11 +1,4 @@
-/* Copyright (C) 2020 Yusuf Usta.
-
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-*/
-
+// Copyright (C) 2020 Afx-Abu //
 module.exports = {
     Base: require('./Base'),
     Message: require('./Message'),
